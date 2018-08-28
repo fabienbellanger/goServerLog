2321e9cd651bb1305b2b49f7b3451c44520eaeaa
Fabien Bellanger <valentil@gmail.com>
2018-08-28 23:29:34 +0200

    Avancement

--- shared/lib/nginxLogs.go
+++ shared/lib/nginxLogs.go
@@ -2,11 +2,12 @@ package lib
 
 import (
 	"bufio"
-	"log"
+	"fmt"
 	"os"
 	"regexp"
 	"time"
 
+	"github.com/fabienbellanger/goServerLog/shared/model"
 	"github.com/fabienbellanger/goServerLog/shared/toolbox"
 )
 
@@ -27,14 +28,25 @@ func GetNginxLogs() {
 	// Initialisation
 	// --------------
 	initSettings()
+
+	// Récupération des logs
+	// ---------------------
+	logsTmp := getlogsFromFiles()
+	fmt.Println(logsTmp)
+
+	// Traitement des logs
+	// -------------------
+	// var logs []model.Log
+}
+
+// getlogsFromFiles gets logs from files
+func getlogsFromFiles() []model.Log {
 	yesterdayDate := time.Now().AddDate(0, 0, -1).Format(nginxSettings.DateFormat)
-	log.Println(yesterdayDate)
+	logs := make([]model.Log, 0)
 
 	// Récupération des fichiers de logs
 	// ---------------------------------
 	for _, project := range projects {
-		log.Println(project.Name)
-
 		// Ouverture du fichier
 		// --------------------
 		file, err := os.Open(nginxSettings.LogsPaths + "/" + project.Name + nginxSettings.FileSuffix)
@@ -45,11 +57,9 @@ func GetNginxLogs() {
 		// ------------------
 		lines := make(map[Project][]string)
 		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			// GREP Regex : "(\d{4}\/\d{2}\/\d{2})\s(\d{2}:\d{2}:\d{2}).*(?:PHP Fatal error:  |PHP Warning:  |timed out |No database selected )(?:(.*)(?:, client: )(.*)(?:, server: )(.*)(?:, request: )(.*)(?:, upstream: )(.*)(?:, host: )(.*)|(.*))"
 
-			// TODO: regexPattern := regexp.MustCompile(`(?m)` + yesterdayDate + `.*(Fatal error|Warning|timed out|No database selected).*`)
-			regexPattern := regexp.MustCompile(`(?m).*(Fatal error|Warning|timed out|No database selected).*`)
+		for scanner.Scan() {
+			regexPattern := regexp.MustCompile(`(?m)` + yesterdayDate + `.*(Fatal error|Warning|timed out|No database selected).*`)
 			line := scanner.Text()
 			regexResult := regexPattern.FindAllSubmatch([]byte(line), -1)
 
@@ -59,11 +69,45 @@ func GetNginxLogs() {
 
 				// On parse la ligne pour récupérer les infos utiles
 				// -------------------------------------------------
+				regexPattern = regexp.MustCompile(`(\d{4}\/\d{2}\/\d{2})\s(\d{2}:\d{2}:\d{2}).*(?:PHP Fatal error:  |PHP Warning:  |timed out |No database selected )(?:(.*)(?:, client: )(.*)(?:, server: )(.*)(?:, request: )(.*)(?:, upstream: )(.*)(?:, host: )(.*)|(.*))`)
+				regexResult = regexPattern.FindAllSubmatch([]byte(line), -1)
+
+				if len(regexResult) == 1 && len(regexResult[0]) == 10 {
+					currentIndex := 0
+					linesNumber := len(logs)
+
+					for !(currentIndex == linesNumber || logs[currentIndex].Message == string(regexResult[0][9])) {
+						currentIndex++
+					}
+					fmt.Println(currentIndex, linesNumber)
+					if currentIndex == linesNumber {
+						// Pas de message trouvé, on ajoute au tableau
+						log := model.Log{
+							Project:   project.Name,
+							Server:    "",
+							Number:    1,
+							Message:   string(regexResult[0][9]),
+							HourStart: string(regexResult[0][2]),
+							HourEnd:   string(regexResult[0][2]),
+						}
+
+						logs = append(logs, log)
+					} else {
+
+						logs[currentIndex].Number++
+						logs[currentIndex].HourEnd = string(regexResult[0][2])
+					}
+				}
 			}
 		}
-
-		log.Println(len(lines[project]))
 	}
+	fmt.Println(len(logs))
+	return logs
+}
 
-	log.Println(nginxSettings, projects)
+// prepareFinalLogs prepare log before sending
+func prepareFinalLogs(logs []model.Log) []model.Log {
+	finalLogs := make([]model.Log, 0)
+
+	return finalLogs
 }
